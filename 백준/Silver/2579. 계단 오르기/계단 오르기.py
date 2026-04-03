d70e37fe17c21ece52565fdadb5862a6422102bb
Mr124 <149123411+mister124@users.noreply.github.com>
2026-04-03 20:32:35 +0900

    [Silver III] Title: 계단 오르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+score = [int(input()) for _ in range(N)]
+
+if N == 1:
+    print(score[0])
+elif N == 2:
+    print(score[0] + score[1])
+else:
+    dp = [0] * N
+    dp[0] = score[0]
+    dp[1] = score[0] + score[1]
+    dp[2] = max(score[0] + score[2], score[1] + score[2])
+    
+    for i in range(3, N):
+        dp[i] = max(dp[i-2] + score[i], dp[i-3] + score[i-1] + score[i])
+        
+    print(dp[-1])
